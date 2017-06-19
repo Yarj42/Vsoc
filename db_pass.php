d37c66f73c44e3f08c0406973bf37cb3a1881466
Vsoc <yaroslav.j42@gmail.com>
2017-06-19 15:03:46 +0300

    Add files via upload

---
+++ db_pass.php
@@ -0,0 +1,25 @@
+<?php 
+$data = [
+"admin"=>"admin",
+"user1"=>"user1",
+"user2"=>"user2"
+];
+$str = "Вы не авторизированы!";
+if(isset($_POST["log"]) and isset($_POST["pass"])){
+	if(check($_POST["log"],$_POST["pass"])==true)
+		$str =  "Вы авторизированы!";
+}
+if(isset($_GET["log"]) and isset($_GET["pass"])){
+	if(check($_GET["log"],$_GET["pass"])==true)
+		$str =  "Вы авторизированы!";
+}
+echo $str;
+function check($log,$pass){
+	global $data;
+	$res = false;
+	if(isset($data[$log]))
+		if($data[$log] == $pass)
+			$res = true;
+	return $res;
+}
+?>
